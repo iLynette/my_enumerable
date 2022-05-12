60d1bb3f32e2e4030a0d3e36ba5675fb455c5768
unknown <saied2421998@gmail.com>
2022-05-12 16:17:34 +0200

    create filter method and finish MyEnumerable module

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -10,4 +10,10 @@ module MyEnumerable
 		list.each { |num| if_any = true if yield num }
 		if_any
 	end
+
+	def filter
+	    filtered_arr = []
+	    list.each {|num| filtered_arr.push(num) if yield num}
+	    filtered_arr
+	end     
 end	
