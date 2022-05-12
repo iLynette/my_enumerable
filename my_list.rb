00c39aaddef1f5f0c3663a571589c9327808c2b8
unknown <saied2421998@gmail.com>
2022-05-12 17:06:21 +0200

    Run tests & Fix linter errors

--- my_list.rb
+++ my_list.rb
@@ -1,19 +1,35 @@
-require_relative './my_enumerable.rb'
+require_relative './my_enumerable'
 
 class MyList
-    def initialize(*list)
-        @list = list
-    end
-    include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
+  include MyEnumerable
 
-    def each
-       i=0 
-      while i < @list.length
-         puts "hi"
-         yield @list[i]
-         i+=1
-      end
-    end     
+  def each
+    i = 0
+    while i < @list.length
+      yield @list[i]
+      i += 1
+    end
+  end
 end
 
-test = MyList.new(1,2,3,4)
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+list.all? { |e| e < 5 }
+# true
+list.all? { |e| e > 5 }
+# false
+
+# Test #any?
+list.any? { |e| e == 2 }
+# true
+list.any? { |e| e == 5 }
+# false
+
+# Test #filter
+list.filter(&:even?)
+# [2, 4]
