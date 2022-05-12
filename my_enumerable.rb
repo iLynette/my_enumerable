38e3ab9f43da79c31dd366a7b651edd8e2f52cea
iLynette <lynetteacholah@icloud.com>
2022-05-12 17:10:12 +0300

    Create any? method

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -4,4 +4,10 @@ module MyEnumerable
 		list.each {|num| check_arr.push(num) if yield num} 
         check_arr.length == list.length
 	end	
+
+	def any?
+		if_any = false
+		list.each { |num| if_any = true if yield num }
+		if_any
+	end
 end	
